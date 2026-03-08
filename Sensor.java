7554b5b4cd1abeec199103c6415e53f148dd5839
ljpickart <156261587+ljpickart@users.noreply.github.com>
2026-03-08 19:53:43 -0400

    Update Sensor.java

--- Sensor.java
+++ Sensor.java
@@ -1,6 +1,6 @@
 public class Sensor {
   public static void main(String[] args) {
-    while(1) {
+    while(true) {
       System.out.println("Sensor read: " + Math.random() * 25 + 15);
     }
   }
