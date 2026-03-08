ab904956f32a1bbbcf14937c956ed289b8a8d712
ljpickart <156261587+ljpickart@users.noreply.github.com>
2026-03-08 19:55:52 -0400

    Update Sensor.java

--- Sensor.java
+++ Sensor.java
@@ -1,7 +1,10 @@
 public class Sensor {
   public static void main(String[] args) {
-    while(true) {
+    //Implement a count variable for simulation purposes. Can't have an infinite loop for the sake of the CI/CD pipeline
+    int count = 0;
+    while(count < 200) {
       System.out.println("Sensor read: " + Math.random() * 25 + 15);
+      count++;
     }
   }
 }
