bba31fb272f80e59804b6ccf6017104c7cbc3492
ljpickart <156261587+ljpickart@users.noreply.github.com>
2026-03-08 19:49:31 -0400

    Update sensor.java

--- sensor.java
+++ sensor.java
@@ -1,19 +1,7 @@
 public class Sensor {
   public static void main(String[] args) {
-    System.out.println("Sensor Ready for Readings!");
-    System.out.println("Sensor starting...");
-    System.out.println("Sensor Read: 25");
-    System.out.println("Sensor Read: 27");
-    System.out.println("Sensor Read: 26");
-    System.out.println("Sensor Read: 24");
-    System.out.println("Sensor Read: 30");
-    System.out.println("Sensor Read: 29");
-    System.out.println("Sensor Read: 25");
-    System.out.println("Sensor Read: 24");
-    System.out.println("Sensor Read: 21");
-    System.out.println("Sensor Read: 23");
-    System.out.println("Sensor Read: 25");
-    System.out.println("Sensor Read: 26");
-    System.out.println("Sensor Finished Testing: Forced Stop");
+    while(1) {
+      System.out.println("Sensor read: " + Math.random() * 25 + 15);
+    }
   }
 }
