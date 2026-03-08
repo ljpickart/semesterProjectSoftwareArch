19f390684d4db958fb7910b824e68a1e10804006
ljpickart <156261587+ljpickart@users.noreply.github.com>
2026-03-08 19:43:14 -0400

    Update sensor.java

--- sensor.java
+++ sensor.java
@@ -1,5 +1,19 @@
 public class Sensor {
   public static void main(String[] args) {
     System.out.println("Sensor Ready for Readings!");
+    System.out.println("Sensor starting...");
+    System.out.println("Sensor Read: 25");
+    System.out.println("Sensor Read: 27");
+    System.out.println("Sensor Read: 26");
+    System.out.println("Sensor Read: 24");
+    System.out.println("Sensor Read: 30");
+    System.out.println("Sensor Read: 29");
+    System.out.println("Sensor Read: 25");
+    System.out.println("Sensor Read: 24");
+    System.out.println("Sensor Read: 21");
+    System.out.println("Sensor Read: 23");
+    System.out.println("Sensor Read: 25");
+    System.out.println("Sensor Read: 26");
+    System.out.println("Sensor Finished Testing: Forced Stop");
   }
 }
