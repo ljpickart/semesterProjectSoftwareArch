fdfdde5eb8f21406ed7beaf0ee58536870cb2cdd
Ezra Crocker <74689473+G0bbs@users.noreply.github.com>
2026-03-21 14:31:43 -0400

    README update and transformer.py

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# semesterProjectSoftwareArch
+# Software Architecture Semester Project
+
+## Sensor.java
+
+Generates fake voltages as sensor readings
+
+## sampler.js
+
+Node.js file with HTTP endpoints. Takes voltage readings from the sensor
+and samples the data.
+
+## transformer.py
+
+Python flask server with HTTP endpoints. Takes in sampled voltage data from sampler.js
+and converts the voltage data into temperature data.
+
+## Python dependences (transformer.py)
+
+The python flask server runs in a virtual environment with these dependences (from pip freeze)
+
+blinker==1.9.0
+click==8.3.1
+colorama==0.4.6
+Flask==3.1.3
+itsdangerous==2.2.0
+Jinja2==3.1.6
+MarkupSafe==3.0.3
+Werkzeug==3.1.6
--- sampler.js
+++ sampler.js
@@ -10,11 +10,11 @@ let newRequest = null;
 const server = createServer((req, res) => {
 
   res.on('close', () => {
-          if(newRequest && newRequest.res === res) {
-            console.log("Disconnected, request thrown out");
-            newRequest = null;
-          }
-        });
+    if (newRequest && newRequest.res === res) {
+      console.log("Disconnected, request thrown out");
+      newRequest = null;
+    }
+  });
 
   // Taken from https://nodejs.org/en/learn/http/anatomy-of-an-http-transaction#a-quick-thing-about-errors
   let body = [];
@@ -23,7 +23,6 @@ const server = createServer((req, res) => {
       body.push(chunk);
     })
     .on('end', () => {
-      // TODO: process as json instead of a string
       body = Buffer.concat(body).toString();
 
       // Print raw json data if debug option is set
@@ -44,11 +43,11 @@ const server = createServer((req, res) => {
 
       //data coming in from sensor
       // JSON format: {measurement: x}
-      if(req.url === "/") {
+      if (req.url === "/") {
         const dataValue = parseFloat(parsed.measurement);
 
         //sanity checking
-        if(dataValue > 150 || dataValue < -175) {
+        if (dataValue > 150 || dataValue < -175) {
           res.statusCode = 400;
           res.end("Bad measurement from sensor");
           return;
@@ -59,7 +58,7 @@ const server = createServer((req, res) => {
         res.end('Data received.');
 
         //if no current request for data from the sensor, ignore the data
-        if(newRequest == null) {
+        if (newRequest == null) {
           return;
         } else {
           console.log("Request for data from sensor filled");
@@ -68,7 +67,7 @@ const server = createServer((req, res) => {
           newRequest.sampleData.push(dataValue);
 
           //check if all the requested samples have been filled
-          if(newRequest.sampleData.length >= newRequest.numberOfSamples) {
+          if (newRequest.sampleData.length >= newRequest.numberOfSamples) {
             const numberOfSamples = newRequest.numberOfSamples;
             const sampleData = newRequest.sampleData;
             const returnRes = newRequest.res;
@@ -76,26 +75,25 @@ const server = createServer((req, res) => {
 
             returnRes.statusCode = 200;
             returnRes.setHeader('Content-Type', 'application/json');
-            returnRes.end(JSON.stringify({sampleSuccessful: 1, responseMessage: "Success", numberOfSamples, sampleData}))
+            returnRes.end(JSON.stringify({ sampleSuccessful: 1, responseMessage: "Success", numberOfSamples, sampleData }))
           }
         }
 
         //POST /sample endpoint, client posts to this endpoint with number of samples and recieves the data
-      } else if(req.url === '/sample') {
+      } else if (req.url === '/sample') {
         const numberOfSamples = parsed.numberOfSamples;
 
-        if(numberOfSamples === null || numberOfSamples < 1) {
+        if (numberOfSamples === null || numberOfSamples < 1) {
           res.statusCode = 400;
           res.setHeader('Content-Type', 'application/json');
-          res.end(JSON.stringify({sampleSuccessful: 0, responseMessage: "Improper number of samples requested"}));
+          res.end(JSON.stringify({ sampleSuccessful: 0, responseMessage: "Improper number of samples requested" }));
           return;
         }
 
-        newRequest = {numberOfSamples, sampleData: [], res};
-        
+        newRequest = { numberOfSamples, sampleData: [], res };
+
       }
     })
-  // at this point, `body` has the entire request body stored in it as a string
 });
 
 server.listen(port, hostname, () => {
